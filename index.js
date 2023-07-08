103e04a5ac246a3687f9a7268a58980243f9e3ec
Dilrabo <dilraboxujamuratova@gmail.com>
2023-07-09 00:09:15 +0500

    if else mashqlari

---
+++ index.js
@@ -0,0 +1,65 @@
+// temp = 5;
+// if (temp < 0){
+//         console.log("xavo juda sovuq");
+// }
+// else if(temp< 10){
+//         console.log("xavo illiq");
+// }
+// else if(temp < 25){
+//         console.log("xavo issiq");
+// }
+// else{
+//         console.log("nomalum ob xavo");
+// }
+
+
+// temp = 22;
+// if(temp < 25 && temp >15){
+//         console.log("xavo illiq");
+// }
+// else if (temp < 15 && temp > 10){
+//         console.log("xavo issiq");
+// }
+// else if ( temp < 10 && temp > 0){
+//         console.log(" xavo juda issiq");
+// }else{
+//         console.log("nomalum ob havo");
+// }
+
+
+
+
+
+
+
+
+//==========
+// day = 7;
+// if( day <= 1 && day > 2)console.log("Dushanbi");
+// else if ( day <= 2 && day < 3)console.log("Seyshanbi");
+// else if ( day <= 3 && day < 4)console.log("Chorshanbi");
+// else if ( day <= 4 && day < 5)console.log("Payshanba");
+// else if ( day <= 5 && day < 6)console.log("Juma muborak");
+// else if ( day <= 6 && day < 7)console.log("Shanbi");
+// else if ( day <= 7 && day < 8)console.log("Yakshanbi");
+// else console.log("not found");
+
+// =================? :
+
+
+// day = 1;
+// (day = 1 && day <2)
+// ?console.log("Dushanbi")
+// :( day <= 2 && day < 3)
+// ?console.log("Seyshanbi")
+// :( day <= 3 && day < 4)
+// ?console.log("Chorshanbi")
+// : ( day <= 4 && day < 5)
+// ?console.log("Payshanba")
+// :( day <= 5 && day < 6)
+// ?console.log("Juma muborak")
+// :( day <= 6 && day < 7)
+// ?console.log("Shanbi")
+// :( day <= 7 && day < 8)
+// ?console.log("Yakshanbi")
+// :0;
